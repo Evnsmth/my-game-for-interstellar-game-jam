f3bbad35459a242dca151e5d06dfe616dae9ad96
Evan Smith <evnsmth6@gmail.com>
2026-08-06 11:36:00 -0400

    State Machine Implemented

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,11 +5,35 @@ extends CharacterBody2D
 @export var deceleration = 800.0
 
 
+enum State {
+	MOVE,
+	DASH,
+	ATTACK
+}
+
+var current_state: State = State.MOVE
+
 func _physics_process(delta: float) -> void:
+
+	match current_state:
+		
+		State.MOVE:
+			move_state(delta)
+		
+		State.DASH:
+			dash_state(delta)
+		
+		State.ATTACK:
+			attack_state(delta)
+
+func change_state(new_state):
+	current_state = new_state
+
+func move_state(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_vector("left", "right","up","down")
-	
+
 	# Increase velocity
 	if direction:
 		velocity = direction * movement_speed
@@ -19,3 +43,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y,0,deceleration * delta)
 
 	move_and_slide()
+
+func dash_state(delta):
+	pass
+
+func attack_state(delta):
+	pass
+ 
