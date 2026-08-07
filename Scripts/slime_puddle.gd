32d99b593b4de2bf7a9c69c86468d4afb38016d8
Evan Smith <evnsmth6@gmail.com>
2026-08-07 09:58:53 -0400

    puddles can't spawn on top of eachother

--- Scripts/slime_puddle.gd
+++ Scripts/slime_puddle.gd
@@ -1,11 +1,12 @@
 class_name SlimePuddle
 extends Node2D
 
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var floor = get_tree().get_first_node_in_group("floor")
 
 @export var slime_effect: SlimeEffect
+@export var puddle_spacing: float = 25.0
 
-@onready var sprite: Sprite2D = $Sprite2D
 
 var player_nearby: Node = null
 
@@ -21,9 +22,10 @@ func _ready() -> void:
 
 	sprite.texture = slime_effect.puddle_texture
 
-	print("Spawned puddle containing: ", slime_effect.effect_name)
+	await get_tree().physics_frame
+	find_free_position()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if player_nearby != null and Input.is_action_just_pressed("consume"):
 		consume()
 
@@ -45,6 +47,32 @@ func consume():
 	puddle_consumed.emit()
 	
 	queue_free()
+
+func find_free_position() -> void:
+	var attempts := 0
+	var max_attempts := 20
+
+	while is_too_close_to_another_puddle() and attempts < max_attempts:
+		global_position += Vector2(
+			randf_range(-puddle_spacing, puddle_spacing),
+			randf_range(-puddle_spacing, puddle_spacing)
+		)
+
+		attempts += 1
+
+func is_too_close_to_another_puddle() -> bool:
+	var puddles = get_tree().get_nodes_in_group("puddle")
+
+	for puddle in puddles:
+		if puddle == self:
+			continue
+
+		var distance = global_position.distance_to(puddle.global_position)
+
+		if distance < puddle_spacing:
+			return true
+
+	return false
 
 func _on_remove_puddles():
 	queue_free()
